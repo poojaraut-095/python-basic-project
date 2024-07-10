446f609f93181a01abca401ab3fab9c6fdb9d254
Pooja Raut <rautpooja095@gmail.com>
2024-07-10 12:04:44 -0500

    f-string .py
    F-string program

---
+++ f-string .py
@@ -0,0 +1,9 @@
+# Python3 program introducing f-string
+val = 'Geeks'
+print(f"{val}for{val} is a portal for {val}.")
+
+
+name = 'Om'
+age = 22
+print(f"Hello, My name is {name} and I'm {age} years old.")
+
